c8cfeb4944c495eb39727eeedd91e5d25b408ec2
Полищук Никита <policyaskrimerov@gmail.com>
2025-12-31 20:13:38 +0300

    Last commit in 2025

--- src/menu/components/logo_renderer.cpp
+++ src/menu/components/logo_renderer.cpp
@@ -13,6 +13,7 @@ void menu_logo::render(){
     int width = 300*gfx::screen_aspect();
     gfx::set_viewport(0,0,screen_width, screen_height);
     gfx::set_ortho(0,0, width,300);
+    // Last commit in 2025 year
 
     gfx::enable_texture(data2d::textures[LOGO]);
     gfx::draw_2d_quad(vec2(0,0), vec2(150,100), vec4(1,1,1,1));

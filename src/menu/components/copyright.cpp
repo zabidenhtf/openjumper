e13411c4ad9b829a2732d81097c3a29d22d239ee
zabidenhtf <policyaskrimerov@gmail.com>
2026-01-20 18:20:30 +0200

    Added copyright to copyright.cpp file

--- src/menu/components/copyright.cpp
+++ src/menu/components/copyright.cpp
@@ -1,3 +1,6 @@
+/* Copyright (C) 2025-2026 Mykyta Polishyk */
+/* This project is licensed under the GNU General Public License v3.0 or later. */
+/* See the LICENSE file for details. */
 #include "copyright.hpp"
 
 menu_copyright::menu_copyright(){
